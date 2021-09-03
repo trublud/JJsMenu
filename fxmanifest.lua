5112166dc82ea2eac32a4ab7547c807d54ac6fd4
trublud <xmindpingx@gmail.com>
2021-09-02 22:19:24 -0700

    Redm JJsMenu

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'bodacious'
+game {'gta5', 'rdr3',}
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+author 'mindping'
+description 'JJsMenu'
+version '0.0.1'
+
+client_script 'JJsRedU.net.dll'
+
+files {
+    'config.ini',
+    'MenuAPI.dll',
+} 
+exports {
+    'CreateVehicle',
+}
